c41982a1b3d86b664453545fc5713a205f4b60bf
Roland <rolandrfuchs92@gmail.com>
2017-10-31 02:06:04 +0200

    Solution to Problem 7

---
+++ SolutionsAssembly/_10001stPrime.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionsAssembly
+{
+	public class _10001stPrime : ISolutionsContract
+	{
+		public string ProblemDescription => "By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.\r\n" +
+																					"What is the 10 001st prime number?";
+		public string ProblemName => "10001st Prime";
+		public int ProblemNumber => 7;
+
+		public int Solution()
+		{
+			return ProblemSolution(10001);
+		}
+
+		private int ProblemSolution(int primeInc)
+		{
+			var ans = 0;
+			for (int inc = 2; ; inc++)
+			{
+				if (IsPrime(inc))
+				{
+					ans++;
+					if (ans == primeInc)
+						return inc;
+				}
+			}
+		}
+
+		private bool IsPrime(int number)
+		{
+			for (var inc = 2; inc <= (number / 2); inc++)
+				if (number % inc == 0)
+					return false;
+
+			return true;
+		}
+
+	}
+}
