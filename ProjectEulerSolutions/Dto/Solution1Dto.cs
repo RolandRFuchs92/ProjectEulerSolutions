5da1eea141114e16dc9512d8ecd766204a2fb5be
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2017-11-01 20:03:17 +0200

    Solution  to Problem 9

--- ProjectEulerSolutions/Dto/Solution1Dto.cs
+++ ProjectEulerSolutions/Dto/Solution1Dto.cs
@@ -7,7 +7,7 @@ namespace ProjectEulerSolutions.Dto
 {
     public class Solution1Dto
     {
-        public int Answer { get; set; }
+        public string Answer { get; set; }
         public string Problem { get; set; }
     }
 }
