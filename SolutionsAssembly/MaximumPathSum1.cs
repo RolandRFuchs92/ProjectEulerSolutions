22ea9509e1196ffc8a288bd9e342c8bcd91fce59
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2017-11-13 21:47:04 +0200

    the long awaited and frustrating solution to problem18

--- SolutionsAssembly/MaximumPathSum1.cs
+++ SolutionsAssembly/MaximumPathSum1.cs
@@ -10,7 +10,9 @@ namespace SolutionsAssembly
 	public class MaximumPathSum1 : ISolutionsContract
 	{
 		public string ProblemName => "Maximum Path Sum 1";
-		public string ProblemDescription => $@"By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+		public string ProblemDescription =>
+			$@"By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
 
 			3
 			7 4
@@ -24,6 +26,7 @@ namespace SolutionsAssembly
 		{_pyramidHolder}
 
 		NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route.However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)";
+
 		public int ProblemNumber => 18;
 
 		//private string _pyramidHolder => 
@@ -34,7 +37,7 @@ namespace SolutionsAssembly
 
 
 		private string _pyramidHolder =>
-		@"75
+			@"75
 			95 64
 			17 47 82
 			18 35 87 10
@@ -55,135 +58,64 @@ namespace SolutionsAssembly
 		 * so link is awesome cause you can use it to effectively query arrays...
 		 * i prefer this method as it looks neater and cleaner but the otherway looks more like SQL and i like SQL...
 		 */
-		 private int[][] _pyramidArray =>
-					_pyramidHolder
-							.Split('\n')
-							.Select(
-									t => t.Trim()
-									.Split(' ')
-									.Select(int.Parse)
-									.ToArray()
-							).ToArray();
+		private int[][] _pyramidArray =>
+			_pyramidHolder
+				.Split('\n')
+				.Select(
+					t => t.Trim()
+						.Split(' ')
+						.Select(int.Parse)
+						.ToArray()
+				).ToArray();
 
 
 		/*
 		 this is a second way you could use link.... query an array as if it was a database object... freaking awesome!
 			 */
 		private string[][] _2pyramidArray => (from t in _pyramidHolder.Split('\n')
-																					select (from i in t.Split(' ')
-																									select i).ToArray()
-																					).ToArray();
+			select (from i in t.Split(' ')
+				select i).ToArray()
+		).ToArray();
 
 
 		public string Solution()
 		{
-			return SolutionProblem().ToString();
+			return ProblemSolution().ToString();
 		}
 
-		private int SolutionProblem()
+		private int ProblemSolution()
 		{
-			int colPos = 0;
-			int ans = _pyramidArray[0][0];
+			return GetMaxPath();
+		}
 
-			for (int row = 0; row < _pyramidArray.Length; row++)	//each row
+		private int GetMaxPath()
+		{
+			int[] stackAns = new int[_pyramidArray.Length - 1];
+			stackAns = _pyramidArray[_pyramidArray.Length - 1];
+
+			for (int row = _pyramidArray.Length-2; row > 0; row--)
+				stackAns = GetStackedArray(stackAns, _pyramidArray[row]); //return new stacked array
+
+			return stackAns.Max() + _pyramidArray[0][0];// add the first row to the stack.
+		}
+
+
+
+		private int[] GetStackedArray(int[] stackAns, int[]compareRow)
+		{
+			int []newStackArray = new int[stackAns.Length-1]; //nice clean array
+
+			for (int col = 0; col < stackAns.Length-1; col++)//the row above so, row -1
 			{
+				int checkA = stackAns[col];			//left point of a fork 
+				int checkB = stackAns[col+1];		//right poiint of fork
 
-				int[][] triRows = new int [4][];										//set triangle rows
-				triRows[0] = _pyramidArray[row];
-				triRows[1] = _pyramidArray[row + 1];
-				triRows[2] = _pyramidArray[row + 2];
-				triRows[3] = _pyramidArray[row + 3];
-
-				int [][] miniTri =  GetMiniTri(triRows, colPos);		//get the miniTri
-																																						//int permuTation = (int)Math.Pow(2, miniTri.Length - 1);
-				int[] permutationArray = GetPermuations(miniTri);
-				colPos += GetNewColPosition(permutationArray);
-				Console.WriteLine($"{colPos} - {_pyramidArray[row + 1][colPos]}"  );
-				if ((row + 4) >= _pyramidArray.Length)
-					return ans += permutationArray.Max();
-																																						//get permuations of tri
-																																						//pick biggest...
-				ans += _pyramidArray[row+1][colPos];
+				newStackArray[col] = checkA > checkB //newStackArray > if left for is greater then right fork
+														? compareRow[col] + checkA  //stack left fork onto the new row
+														: compareRow[col] + checkB; //stack right fork onto the new row
 			}
-
-			return ans;
-		}
-
-
-		private int[] GetPermuations(int [][] miniTri)
-		{
-			//int [] permutations = new int[(int)Math.Pow(2, miniTri.Length -1)];
-			int [] permutations = new int[miniTri.Length -1];
-
-			permutations[0] = miniTri[0][0] + miniTri[1][0] + miniTri[2][0];
-			permutations[1] = miniTri[0][0] + miniTri[1][0] + miniTri[2][1];
-			permutations[2] = miniTri[0][0] + miniTri[1][1] + miniTri[2][1];
-			permutations[3] = miniTri[0][0] + miniTri[1][1] + miniTri[2][2];
-
-
-			//Work out how to do this programatically dont do it with hardcode...
-			//permutations[0] = miniTri[0][0] + miniTri[1][0] + miniTri[2][0] + miniTri[3][0];
-			//permutations[1] = miniTri[0][0] + miniTri[1][0] + miniTri[2][0] + miniTri[3][1];
-			//permutations[2] = miniTri[0][0] + miniTri[1][0] + miniTri[2][1] + miniTri[3][1];
-			//permutations[3] = miniTri[0][0] + miniTri[1][0] + miniTri[2][1] + miniTri[3][1];
-			//permutations[4] = miniTri[0][0] + miniTri[1][1] + miniTri[2][1] + miniTri[3][2];
-			//permutations[5] = miniTri[0][0] + miniTri[1][1] + miniTri[2][1] + miniTri[3][2];
-			//permutations[6] = miniTri[0][0] + miniTri[1][1] + miniTri[2][2] + miniTri[3][2];
-			//permutations[7] = miniTri[0][0] + miniTri[1][1] + miniTri[2][2] + miniTri[3][3];
-
-			return permutations;
-		}
-
-
-
-		private int GetNewColPosition(int []permutionArray)
-		{
-			int colPos = 0,
-					temp = 0, 
-					current = 0, 
-					permuLength = permutionArray.Length-1;
-
-			temp = permutionArray[0];
-
-			for (int inc = 0; inc < permuLength+1; inc++)
-			{
-				if (permutionArray[inc] > temp)
-				{
-					temp = permutionArray[inc];
-					if (inc > permuLength)
-						return 1;
-				}
-			}
-			return 0;
-		}
-
-		
-		/// <summary>
-		/// creates a mini triangle from the given 2D Array
-		/// </summary>
-		/// <param name="triRows">2D array</param>
-		/// <param name="colPos">Starting column in the 2D array</param>
-		/// <returns></returns>
-		private int[][] GetMiniTri(int[][] triRows, int colPos)
-		{
-			int [][]miniTri = new int[triRows.Length][];
-
-			for (int row = 0; row < triRows.Length; row++) //each row
-			{
-				int [] InitRow = new int[triRows[row].Length];
-				for (int col = 0; col < (triRows[row].Length - colPos); col++) //columns from column position => length - column position to avoid out of range excep
-				{
-					InitRow[col] = triRows[row][colPos + col]; //init rows 
-				}
-				miniTri[row]= InitRow; //setup miniTri
-			}
-			return miniTri;
+			return newStackArray; //return the clean array
 		}
 	}
 
-	class PosModel
-	{
-		public int colPos { get; set; }
-		public int ans { get; set; }
-	}
 }
