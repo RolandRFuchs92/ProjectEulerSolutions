19650322b4258f3deebcc06f85b6e9f4326bfb26
Roland Fuchs <rolandrfuchs92@gmail.com>
2017-11-24 18:39:53 +0200

    Added the NameScores.cs file

---
+++ SolutionsAssembly/NameScores.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionsAssembly
+{
+    public class NameScores : ISolutionsContract
+    {
+        public string ProblemName { get; }
+        public string ProblemDescription { get; }
+        public int ProblemNumber { get; }
+
+        public string Solution()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
