a3a531c6309f6c32fc1a435e7c8c03f136f31d5a
Roland Fuchs <rolandrfuchs92@gmail.com>
2017-11-24 20:55:33 +0200

    Added .Net Changes

--- SolutionsAssemblyConsoleTest/SolutionList.cs
+++ SolutionsAssemblyConsoleTest/SolutionList.cs
@@ -36,6 +36,7 @@ namespace SolutionsAssemblyConsoleTest
 			solutionList.Add(new CountingSundays());
 			solutionList.Add(new FactorialDigitSum());
 			solutionList.Add(new AmicableNumbers());
+            solutionList.Add(new NameScores());
         }
 
     }
